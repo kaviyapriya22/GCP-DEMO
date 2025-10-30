6796d3a23a290d70c5795feba2e184fad96a976a
Kaviyapriya K <kaviyapriyak22@gmail.com>
2025-10-30 11:55:15 +0530

    Initial commit

---
+++ src/main/java/com/example/demo_gcp/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo_gcp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/demo")
+public class DemoController {
+
+    @GetMapping
+    public String getMessage() {
+        return "Application is running...";
+    }
+}
